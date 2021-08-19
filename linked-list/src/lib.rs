a39ed6bc996a5b939a28cee00d9cabd588b63ab7
John Ballesteros <johnshift.dev@gmail.com>
2021-08-20 02:45:12 +0800

    ds: linked-list: insert

--- linked-list/src/lib.rs
+++ linked-list/src/lib.rs
@@ -30,7 +30,7 @@ struct LinkedList<T> {
   length: u64,
 }
 
-impl<T> LinkedList<T> {
+impl<T: std::fmt::Debug> LinkedList<T> {
   fn new(value: T) -> Self {
     let init_node = Node::new_with_sptr(value);
 
@@ -69,6 +69,50 @@ impl<T> LinkedList<T> {
 
     // increment length
     self.length += 1;
+  }
+
+  fn insert(&mut self, index: u64, value: T) {
+    // 2 -> 16 -> None
+    // insert(1, 69) =======> 2 -> 69 -> 16 -> None
+    // insert(0, 69) =======> 0 -> 2 -> 16 -> None
+    // insert(2, 69) =======> 2 -> 16 -> 69 -> None
+    // insert(3, 69) =======> panic!
+
+    if index > self.length {
+      panic!("Index out of bounds!");
+    }
+
+    if index == 0 {
+      self.prepend(value);
+      return;
+    }
+
+    if index == self.length {
+      self.append(value);
+      return;
+    }
+
+    // we start with head
+    // while counter != index
+    // proceed to current_node.next
+    // increment counter
+
+    let mut counter = 0_u64;
+    let mut current_node = Rc::clone(&self.head);
+    let mut prev_node = Rc::clone(&current_node);
+    while counter != index {
+      if let Some(some_next) = &current_node.to_owned().borrow().next {
+        prev_node = Rc::clone(&current_node);
+        current_node = Rc::clone(some_next);
+      }
+
+      counter += 1;
+    }
+
+    let new_node = Node::new_with_sptr(value);
+    new_node.borrow_mut().next = Some(Rc::clone(&current_node));
+
+    prev_node.borrow_mut().next = Some(Rc::clone(&new_node));
   }
 }
 
@@ -110,5 +154,106 @@ mod tests {
       }
       None => panic!("Should not return None!"),
     };
+  }
+
+  #[test]
+  fn test_linked_list_insert_0() {
+    // 2 -> 16 -> None
+    // insert(1, 69) =======> 2 -> 69 -> 16 -> None
+    // insert(0, 69) =======> 0 -> 2 -> 16 -> None
+    // insert(2, 69) =======> 2 -> 16 -> 69 -> None
+    // insert(3, 69) =======> panic!
+
+    let mut my_linked_list = LinkedList::new(2);
+    my_linked_list.append(16);
+    my_linked_list.insert(0, 69);
+
+    // test head
+    assert_eq!(my_linked_list.head.borrow_mut().value, 69);
+
+    // test next to head
+    match &my_linked_list.head.borrow().next {
+      Some(sptr) => {
+        assert_eq!(sptr.borrow().value, 2);
+      }
+      None => panic!("Should not return None!"),
+    };
+
+    // test tail
+    assert_eq!(my_linked_list.tail.borrow_mut().value, 16);
+    match my_linked_list.tail.borrow().next {
+      Some(_) => panic!("Should return None!"),
+      None => {}
+    };
+  }
+
+  #[test]
+  fn test_linked_list_insert_1() {
+    // 2 -> 16 -> None
+    // insert(1, 69) =======> 2 -> 69 -> 16 -> None
+    // insert(0, 69) =======> 0 -> 2 -> 16 -> None
+    // insert(2, 69) =======> 2 -> 16 -> 69 -> None
+    // insert(3, 69) =======> panic!
+
+    let mut my_linked_list = LinkedList::new(2);
+    my_linked_list.append(16);
+    my_linked_list.insert(1, 69);
+
+    // test head
+    assert_eq!(my_linked_list.head.borrow_mut().value, 2);
+
+    // test insert_1
+    match &my_linked_list.head.borrow().next {
+      Some(sptr) => {
+        assert_eq!(sptr.borrow().value, 69);
+      }
+      None => panic!("Should not return None!"),
+    };
+
+    // test tail
+    assert_eq!(my_linked_list.tail.borrow_mut().value, 16);
+    match my_linked_list.tail.borrow().next {
+      Some(_) => panic!("Should return None!"),
+      None => {}
+    };
+  }
+
+  #[test]
+  fn test_linked_list_insert_2() {
+    // 2 -> 16 -> None
+    // insert(1, 69) =======> 2 -> 69 -> 16 -> None
+    // insert(0, 69) =======> 0 -> 2 -> 16 -> None
+    // insert(2, 69) =======> 2 -> 16 -> 69 -> None
+    // insert(3, 69) =======> panic!
+
+    let mut my_linked_list = LinkedList::new(2);
+    my_linked_list.append(16);
+    my_linked_list.insert(2, 69);
+
+    // test head
+    assert_eq!(my_linked_list.head.borrow_mut().value, 2);
+
+    // test next to head
+    match &my_linked_list.head.borrow().next {
+      Some(sptr) => {
+        assert_eq!(sptr.borrow().value, 16);
+      }
+      None => panic!("Should not return None!"),
+    };
+
+    // test tail
+    assert_eq!(my_linked_list.tail.borrow_mut().value, 69);
+    match my_linked_list.tail.borrow().next {
+      Some(_) => panic!("Should return None!"),
+      None => {}
+    };
+  }
+
+  #[test]
+  #[should_panic(expected = "Index out of bounds!")]
+  fn test_linked_list_insert_3() {
+    let mut my_linked_list = LinkedList::new(2);
+    my_linked_list.append(16);
+    my_linked_list.insert(99, 69);
   }
 }
