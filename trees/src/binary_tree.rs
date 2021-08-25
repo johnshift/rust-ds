4d0eb5cce1c49d5ec54312864f1ba295722b6432
John Ballesteros <johnshift.dev@gmail.com>
2021-08-26 02:50:27 +0800

    stuck on while loop smart pointers

--- trees/src/binary_tree.rs
+++ trees/src/binary_tree.rs
@@ -1,11 +1,31 @@
+use std::borrow::BorrowMut;
+use std::cell::RefCell;
 use std::rc::Rc;
 
-type Link<T> = Option<Rc<Node<T>>>;
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
 
 struct Node<T> {
   value: T,
   left: Link<T>,
   right: Link<T>,
+}
+
+impl<T> Node<T> {
+  fn new(value: T) -> Self {
+    Self {
+      value,
+      left: None,
+      right: None,
+    }
+  }
+
+  fn assign_left(&mut self, link: Link<T>) {
+    self.left = link;
+  }
+
+  fn assign_right(&mut self, link: Link<T>) {
+    self.right = link;
+  }
 }
 
 struct BinarySearchTree<T> {
@@ -18,17 +38,51 @@ impl<T: PartialOrd> BinarySearchTree<T> {
   }
 
   fn insert(&mut self, value: T) {
-    // if empty tree
+    // first insert
     if self.root.is_none() {
-      self.root = Some(Rc::new(Node {
-        value,
-        left: None,
-        right: None,
-      }));
+      self.root = Some(Rc::new(RefCell::new(Node::new(value))));
       return;
     }
 
     let mut current_node = self.root.clone();
+    'outer: loop {
+      if let Some(ref node) = current_node {
+        if &value < &node.borrow().value {
+          // left
+
+          match &node.borrow().left {
+            None => {
+              (*node.as_ref().borrow_mut())
+                .left
+                .replace(Rc::new(RefCell::new(Node {
+                  value,
+                  left: None,
+                  right: None,
+                })));
+              break 'outer;
+            }
+            Some(n) => {
+              // let ambot = (*n.as_ref().borrow_mut()).left;
+              // current_node.replace((*n.as_ref().borrow_mut()).left.as_ref().unwrap());
+              current_node = (*n.as_ref().borrow_mut()).left;
+            }
+          };
+          // if node.borrow().left.is_none() {
+          //   (*node.as_ref().borrow_mut())
+          //     .left
+          //     .replace(Rc::new(RefCell::new(Node {
+          //       value,
+          //       left: None,
+          //       right: None,
+          //     })));
+
+          //   break 'outer;
+          // } else {
+          //   current_node = (*node.as_ref().borrow_mut()).left;
+          // }
+        }
+      }
+    }
   }
 
   // simply returns the node or None
@@ -43,7 +97,7 @@ mod test {
   use super::*;
 
   #[test]
-  fn btnode_basics() {
+  fn bst_basics() {
     //         9
     //    4        20
     //  1   6   15    170
@@ -52,11 +106,11 @@ mod test {
     // todo: check root is None
     tree.insert(9);
     // todo: check root is 9
-    tree.insert(4);
-    tree.insert(6);
-    tree.insert(20);
-    tree.insert(170);
-    tree.insert(15);
-    tree.insert(1);
+    // tree.insert(4);
+    // tree.insert(6);
+    // tree.insert(20);
+    // tree.insert(170);
+    // tree.insert(15);
+    // tree.insert(1);
   }
 }
