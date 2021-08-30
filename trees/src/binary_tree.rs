37610be2a5cfed0816b807ae8b3d68d0d7b35e7d
John Ballesteros <johnshift.dev@gmail.com>
2021-08-30 22:12:45 +0800

    tree: has lookup fn

--- trees/src/binary_tree.rs
+++ trees/src/binary_tree.rs
@@ -38,6 +38,22 @@ impl<T: Ord> Tree<T> {
 
     current.0 = Some(Box::new(Node::new(value)));
   }
+
+  fn has(&self, value: T) -> bool {
+    let mut current = self;
+
+    while let Some(ref node) = current.0 {
+      match node.value.cmp(&value) {
+        Ordering::Equal => {
+          return true;
+        }
+        Ordering::Greater => current = &node.right,
+        Ordering::Less => current = &node.left,
+      }
+    }
+
+    false
+  }
 }
 
 #[cfg(test)]
@@ -45,7 +61,7 @@ mod tests {
   use super::*;
 
   #[test]
-  fn bst_basics() {
+  fn bst_insert() {
     //         9
     //    4        20
     //  1   6   15    170
@@ -59,5 +75,28 @@ mod tests {
     tree.insert(170);
 
     println!("{:#?}", tree);
+  }
+
+  #[test]
+  fn bst_has() {
+    let mut tree = Tree::new(9);
+    tree.insert(4);
+    tree.insert(20);
+    tree.insert(1);
+    tree.insert(6);
+    tree.insert(15);
+    tree.insert(170);
+
+    assert!(tree.has(9));
+    assert!(tree.has(4));
+    assert!(tree.has(20));
+    assert!(tree.has(1));
+    assert!(tree.has(6));
+    assert!(tree.has(15));
+    assert!(tree.has(170));
+
+    assert!(!tree.has(99));
+    assert!(!tree.has(0));
+    assert!(!tree.has(44));
   }
 }
