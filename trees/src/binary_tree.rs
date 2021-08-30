9280c2a8141e2cd3a09591a157bd3dd7e5c72884
John Ballesteros <johnshift.dev@gmail.com>
2021-08-30 19:26:06 +0800

    tuple struct tree: insert

--- trees/src/binary_tree.rs
+++ trees/src/binary_tree.rs
@@ -1,99 +1,47 @@
-use std::borrow::BorrowMut;
-use std::cell::RefCell;
-use std::rc::Rc;
+use std::cmp::{Ord, Ordering};
 
-type Link<T> = Option<Rc<RefCell<Node<T>>>>;
-
-struct Node<T> {
+#[derive(Debug)]
+struct Node<T: Ord> {
   value: T,
-  left: Link<T>,
-  right: Link<T>,
+  left: Tree<T>,
+  right: Tree<T>,
 }
 
-impl<T> Node<T> {
+impl<T: Ord> Node<T> {
   fn new(value: T) -> Self {
     Self {
       value,
-      left: None,
-      right: None,
+      left: Tree(None),
+      right: Tree(None),
     }
   }
-
-  fn assign_left(&mut self, link: Link<T>) {
-    self.left = link;
-  }
-
-  fn assign_right(&mut self, link: Link<T>) {
-    self.right = link;
-  }
 }
 
-struct BinarySearchTree<T> {
-  root: Link<T>,
-}
+#[derive(Debug)]
+struct Tree<T: Ord>(Option<Box<Node<T>>>);
 
-impl<T: PartialOrd> BinarySearchTree<T> {
-  fn new() -> Self {
-    Self { root: None }
+impl<T: Ord> Tree<T> {
+  fn new(value: T) -> Self {
+    Self(Some(Box::new(Node::new(value))))
   }
 
   fn insert(&mut self, value: T) {
-    // first insert
-    if self.root.is_none() {
-      self.root = Some(Rc::new(RefCell::new(Node::new(value))));
-      return;
-    }
+    let mut current = self;
 
-    let mut current_node = self.root.clone();
-    'outer: loop {
-      if let Some(ref node) = current_node {
-        if &value < &node.borrow().value {
-          // left
-
-          match &node.borrow().left {
-            None => {
-              (*node.as_ref().borrow_mut())
-                .left
-                .replace(Rc::new(RefCell::new(Node {
-                  value,
-                  left: None,
-                  right: None,
-                })));
-              break 'outer;
-            }
-            Some(n) => {
-              // let ambot = (*n.as_ref().borrow_mut()).left;
-              // current_node.replace((*n.as_ref().borrow_mut()).left.as_ref().unwrap());
-              current_node = (*n.as_ref().borrow_mut()).left;
-            }
-          };
-          // if node.borrow().left.is_none() {
-          //   (*node.as_ref().borrow_mut())
-          //     .left
-          //     .replace(Rc::new(RefCell::new(Node {
-          //       value,
-          //       left: None,
-          //       right: None,
-          //     })));
-
-          //   break 'outer;
-          // } else {
-          //   current_node = (*node.as_ref().borrow_mut()).left;
-          // }
-        }
+    while let Some(ref mut node) = current.0 {
+      match node.value.cmp(&value) {
+        Ordering::Equal => current = &mut node.right,
+        Ordering::Greater => current = &mut node.right,
+        Ordering::Less => current = &mut node.left,
       }
     }
-  }
 
-  // simply returns the node or None
-  fn lookup(&mut self) -> Link<T> {
-    unimplemented!()
+    current.0 = Some(Box::new(Node::new(value)));
   }
 }
 
 #[cfg(test)]
-mod test {
-
+mod tests {
   use super::*;
 
   #[test]
@@ -102,15 +50,14 @@ mod test {
     //    4        20
     //  1   6   15    170
 
-    let mut tree = BinarySearchTree::new();
-    // todo: check root is None
-    tree.insert(9);
-    // todo: check root is 9
-    // tree.insert(4);
-    // tree.insert(6);
-    // tree.insert(20);
-    // tree.insert(170);
-    // tree.insert(15);
-    // tree.insert(1);
+    let mut tree = Tree::new(9);
+    tree.insert(4);
+    tree.insert(20);
+    tree.insert(1);
+    tree.insert(6);
+    tree.insert(15);
+    tree.insert(170);
+
+    println!("{:#?}", tree);
   }
 }
